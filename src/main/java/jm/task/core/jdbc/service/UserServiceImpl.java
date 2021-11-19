5d37e76fde5e87a0de7e285752cba0b37ee0218c
mmr3501 <mmr3501@yandex.ru>
2021-11-19 18:18:11 +0300

    !

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,32 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    private final UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
     public void createUsersTable() {
-
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-
+        userDao.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id) throws SQLException {
-
+    public void removeUserById(long id) {
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() throws SQLException {
-       return null;
+       return userDao.getAllUsers();
     }
 
-    public void cleanUsersTable(){
-
+    public void cleanUsersTable() {
+        userDao.cleanUsersTable();
     }
 }
 
